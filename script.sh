43fbc66cf86eccc95d3280ea78f2234bb0cd9555
Randy Murrish <randy@stad.us>
2021-12-13 09:41:19 -0700

    update the script for lacework

--- script.sh
+++ script.sh
@@ -8,7 +8,14 @@ docker build --pull --rm -f "Dockerfile" -t example:latest "."
 
 echo "--- lacework scanner"
 lw-scanner version
-lw-scanner image evaluate example latest --scan-library-packages --verbose --fixable
+
+echo "--- lacework evaluate"
+echo lw-scanner image scan example latest --scan-library-packages --verbose --debug
+lw-scanner image scan example latest --scan-library-packages --verbose --debug
+
+echo "--- lacework evaluate"
+echo lw-scanner image evaluate example latest --fixable --scan-library-packages --verbose --debug
+lw-scanner image evaluate example latest --fixable --scan-library-packages --verbose --debug
 
 echo "--- trivy scanner"
 trivy --version
